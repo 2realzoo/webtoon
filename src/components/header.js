d9b2344c5e125991784c3455bcb311fd3a92f8c3
hyunhoh <skins0329@gmail.com>
2023-01-21 15:59:27 +0900

    header component

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <>
+      <header>
+        <span>profile</span>
+        <span>icon</span>
+        <span>search</span>
+      </header>
+    </>
+  );
+};
+
+export default Header;

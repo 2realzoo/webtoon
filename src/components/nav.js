cc96a6e258d91eab28c403d65533986ffff9a9d6
hyunhoh <skins0329@gmail.com>
2023-01-21 16:02:02 +0900

    nav component

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
 
 function Nav() {
-    
+  return (
+    <>
+      <nav>
+        <span>월</span>
+        <span>화</span>
+        <span>수</span>
+        <span>목</span>
+        <span>금</span>
+        <span>토</span>
+        <span>일</span>
+        <span>filter</span>
+      </nav>
+    </>
+  );
 }
 
 export default Nav;

8e1252b6341c0f73fd8170413884990a49f7adc4
hyunhoh <skins0329@gmail.com>
2023-01-29 18:05:42 +0900

    input autofocus

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,23 @@
 import { FaSearch } from 'react-icons/fa';
 import { FaUser } from 'react-icons/fa';
 // import { logo } from '../../public/img/logo.png';
-import { HiMoon, HiSun } from "react-icons/hi";
+import { HiMoon, HiSun } from 'react-icons/hi';
 import { useDarkMode } from './DarkMode/DarkMode';
 import { useState } from 'react';
 import { webtoons } from '../repository/webtoons';
-import styles from './header.module.css'
+import styles from './header.module.css';
 
 const Header = ({ setFilteredData }) => {
   const [inputView, setInputView] = useState(false);
-  const {darkMode, toggleDarkMode} = useDarkMode();
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
   const handleSearch = e => {
     setFilteredData(webtoons.filter(ele => ele.title.includes(e.target.value)));
   };
 
   return (
     <>
-      <span onClick={toggleDarkMode}>
-        {!darkMode ? <HiMoon /> : <HiSun />}
-      </span>
+      <span onClick={toggleDarkMode}>{!darkMode ? <HiMoon /> : <HiSun />}</span>
       <header className={styles.header}>
         <span className="profile-icon">
           <FaUser></FaUser>
@@ -29,6 +28,7 @@ const Header = ({ setFilteredData }) => {
             <input
               type="text"
               onChange={handleSearch}
+              autoFocus
               onBlur={() => setInputView(false)}></input>
           ) : (
             <FaSearch></FaSearch>
